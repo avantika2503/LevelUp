5bb9cded014edecf836993eca6a232422c993f7a
avantika2503 <avantiika.yadav@gmail.com>
2025-10-24 17:39:43 +0530

    Initial Spring Boot project

---
+++ src/test/java/com/example/LevelUp/LevelUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LevelUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
